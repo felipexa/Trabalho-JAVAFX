df47676fb053f25d70b6c1503b81b4d2ee704f8b
Felipe Xavier <felipexavierass@gmail.com>
2020-09-21 21:03:15 -0300

    ..

---
+++ TRABALHO/src/com/entity/Produto.java
@@ -0,0 +1,57 @@
+package com.entity;
+
+public class Produto {
+
+	private int cod;
+	private String nome;
+	private int quantidade;
+	private Float preco;
+	
+	public Produto() {
+		
+	}
+	public Produto(String nome, int quantidade, Float preco) {
+		super();
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	
+	public Produto(int cod, String nome, int quantidade, Float preco) {
+		super();
+		this.cod = cod;
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	public int getCod() {
+		return cod;
+	}
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Float getPreco() {
+		return preco;
+	}
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+	
+	@Override
+	public String toString() {
+		return "• Nome Produto: " + nome + "\n"  + "• Quantidade: " + quantidade + "\n" + "• Preço: " + preco + "\n" ;
+	}
+	
+}
---
+++ TRABALHO/src/com/principal/Principal.java
@@ -0,0 +1,9 @@
+package com.principal;
+
+import com.view.produto.ControllerProduto;
+
+public class Principal {
+	public static void main(String[] args) {		
+		new ControllerProduto().execute();
+	}
+}
